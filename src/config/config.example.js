891a31b6da58a061e5dd5bd8c0e86f5665e39664
Donn Neufeld <donn.neufeld@gmail.com>
2023-02-22 09:11:06 -0800

    update API keys

--- src/config/config.example.js
+++ src/config/config.example.js
@@ -1,5 +1,5 @@
 // Put your API key goes here!
-var API_KEY = 'YOUR_API_KEY_HERE';
-var YOUTUBE_API_KEY = 'YOUR_YOUTUBE_API_KEY_HERE';
+var API_KEY = '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';
+var YOUTUBE_API_KEY = 'AIzaSyDBJjkU7D5cSqOf2zjeQ7UdO0ooVVm5ErM';
 
 export { API_KEY, YOUTUBE_API_KEY };
